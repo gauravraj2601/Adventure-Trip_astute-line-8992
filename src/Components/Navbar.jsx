f0afc950d75e89535a6403d7489a6c68b3620e9c
gauravraj2601 <gauravraj2601@gmail.com>
2023-06-15 16:19:28 +0530

    db

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import logo from "../Images/logo.png"
+import { Link } from 'react-router-dom'
+import "../Components/Navbar.css"
+
 
 const Navbar = () => {
+  const links=[
+    {
+      path:"/",
+      title:"Home"
+    },
+    {
+      path:"/signin",
+      title:"Sign in"
+    },{
+      path:"/host",
+      title:"Host an Adventure"
+    },
+  ]
   return (
-    <div>Navbar</div>
+    <header class="header" id='header'>
+      <nav class="nav_container" style={{width:"100%",margin:"auto",display:"flex", alignItems:"center",padding:"20px",backgroundColor:"white",boxShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px", textDecoration:"none"}}>
+      <div style={{width:"100px", marginLeft:"50px"}}>
+        <img width="100px" src={logo} alt="Logo" />
+
+      </div>
+      <div className='aaa' style={{width:"50%",margin:"auto",marginRight:"50px",display:"flex", alignItems:"center", justifyContent:"space-around", textDecoration:"none", fontWeight:"700", fontSize:"Large"}}>
+        {links.map((link)=>(
+          <div>
+
+            <Link key={link.path} to={link.path}  >{link.title}</Link>
+          </div>
+        ))}
+      </div>
+          
+      </nav>
+
+    </header>
   )
 }
 
---
+++ src/Pages/Host.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Host = () => {
+  return (
+    <div>Host an Adventure</div>
+  )
+}
+
+export default Host
